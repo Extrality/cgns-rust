f877e5d6e98788f764b64d5947b84ab58be0e692
Arthur Woimbée <arthur.woimbee@gmail.com>
2023-01-17 12:47:16 +0100

    big cleanup of errors.rs

--- cgns-rust/src/errors.rs
+++ cgns-rust/src/errors.rs
@@ -2,65 +2,41 @@ use std::error::Error;
 use std::ffi;
 use std::fmt;
 
-use anyhow::anyhow;
-
 #[derive(Debug, thiserror::Error)]
 pub enum CGNSError {
+    /// Errors coming from the CGNS MLL
     #[error(transparent)]
     CGNSLibError(#[from] CGNSLibraryError),
-    // there is work to do here...
+    /// Error during FFI type conversion
+    #[error("Error in FFI interface: {0}")]
+    FFIError(FFIError),
+    /// Catch-all error type (to phase out)
     #[error(transparent)]
     Other(#[from] anyhow::Error),
 }
 
-impl From<ffi::FromBytesWithNulError> for CGNSError {
-    fn from(err: ffi::FromBytesWithNulError) -> Self {
-        Self::Other(anyhow!(
-            "Could not convert str from bytes: {}",
-            err.to_string()
-        ))
-    }
-}
-
-impl From<ffi::NulError> for CGNSError {
-    fn from(err: ffi::NulError) -> Self {
-        Self::Other(anyhow!(
-            "Could not convert str from bytes: {}",
-            err.to_string()
-        ))
-    }
-}
-
-impl From<std::str::Utf8Error> for CGNSError {
-    fn from(err: std::str::Utf8Error) -> Self {
-        Self::Other(anyhow!(
-            "Could not convert str from bytes: {}",
-            err.to_string()
-        ))
-    }
-}
-
-impl From<std::ffi::FromVecWithNulError> for CGNSError {
-    fn from(err: std::ffi::FromVecWithNulError) -> Self {
-        Self::Other(anyhow!(
-            "Could not convert str from bytes: {}",
-            err.to_string()
-        ))
+/// Allows building a CGNSError from any FFIError member
+impl<E> From<E> for CGNSError
+where
+    FFIError: From<E>,
+{
+    fn from(err: E) -> Self {
+        CGNSError::FFIError(err.into())
     }
 }
 
 #[derive(Debug, thiserror::Error)]
-pub enum FFIStringError {
+pub enum FFIError {
+    #[error(transparent)]
     Null(#[from] ffi::NulError),
+    #[error(transparent)]
     IntoString(#[from] ffi::IntoStringError),
+    #[error(transparent)]
     FromVec(#[from] ffi::FromVecWithNulError),
+    #[error(transparent)]
     FromBytes(#[from] ffi::FromBytesWithNulError),
-}
-
-impl fmt::Display for FFIStringError {
-    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        write!(fmt, "{:?}", self)
-    }
+    #[error(transparent)]
+    Utf8Error(#[from] std::str::Utf8Error),
 }
 
 #[derive(Clone)]
@@ -90,6 +66,35 @@ impl fmt::Display for CGNSLibraryError {
 }
 
 impl Error for CGNSLibraryError {}
-
 unsafe impl Send for CGNSLibraryError {}
 unsafe impl Sync for CGNSLibraryError {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ffi_error_displays_correctly() {
+        let result = ffi::CStr::from_bytes_with_nul(&[b'a', 0, b'b']);
+        let ffi_error = result.unwrap_err();
+        let ffi_error_display = format!("{}", ffi_error);
+        let ffi_error_debug = format!("{:?}", ffi_error);
+        // Check Display on FFIError looks OK
+        assert_eq!(
+            ffi_error_display,
+            "data provided contains an interior nul byte at byte pos 1"
+        );
+        let error: CGNSError = ffi_error.into();
+        let error_display = format!("{}", error);
+        let error_debug = format!("{:?}", error);
+        // Check CGNSError is correctly displaying FFIError
+        assert_eq!(
+            error_display,
+            format!("Error in FFI interface: {}", ffi_error_display)
+        );
+        assert_eq!(
+            error_debug,
+            format!("FFIError(FromBytes({}))", ffi_error_debug)
+        );
+    }
+}
